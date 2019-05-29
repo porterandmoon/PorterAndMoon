0e93502b19041fa3803df9c79ca20814b5c50521
unknown <rys.mary@gmail.com>
2019-05-28 20:55:41 -0500

    both get requests working

---
+++ PorterAndMoon/Connections/OrderProductConnections.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using PorterAndMoon.Models.OrderProduct;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PorterAndMoon.Connections
+{
+    public class OrderProductConnections
+    {
+        readonly string ConnectionString;
+
+        public OrderProductConnections(IOptions<DbConfiguration> dbConfig)
+        {
+            ConnectionString = dbConfig.Value.ConnectionString;
+        }
+
+        public List<OrderProduct> GetAllOrderProduct()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var queryString = @"Select *
+                                    From orderProduct";
+                var products = connection.Query<OrderProduct>(queryString).ToList();
+                return products;
+            }
+        }
+
+        public OrderProduct GetSingleOrderProduct(int id)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var queryString = @"Select *
+                                    From orderProduct
+                                    Where id = @id";
+                var product = connection.QueryFirstOrDefault<OrderProduct>(queryString, new { id });
+                return product;
+            }
+        }
+
+
+    }
+}
---
+++ PorterAndMoon/Controllers/OrderProductController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PorterAndMoon.Connections;
+
+namespace PorterAndMoon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderProductController : ControllerBase
+    {
+        readonly OrderProductConnections _connections;
+
+        public OrderProductController(OrderProductConnections connections)
+        {
+            _connections = connections;
+        }
+
+        [HttpGet("All")]
+        public ActionResult GetAllOrderProducts()
+        {
+            var orderProductsList = _connections.GetAllOrderProduct();
+            return Accepted(orderProductsList);
+        }
+
+        [HttpGet("{Id}")]
+        public ActionResult GetSingleOrderProduct(int Id)
+        {
+            var product = _connections.GetSingleOrderProduct(Id);
+            return Accepted(product);
+        }
+
+
+        //[HttpPost]
+        //public ActionResult AddNewOrderProduct(NewOrderProduct newOrderProduct)
+        //{
+        //    var product = _connections.AddNewOrderProduct(newOrderProduct);
+        //    return Accepted(product);
+        //}
+
+
+        //[HttpDelete("{id}")]
+        //public ActionResult DeleteOrderProduct(int id)
+        //{
+        //    var product = _connections.DeleteOrderProduct(id);
+        //    return Accepted(product);
+        //}
+
+        //[HttpPut]
+        //public ActionResult updateQuantity(OrderProductQuantity updatedOrderProduct)
+        //{
+        //    var product = _connections.UpdateProductQuantity(updatedOrderProduct);
+        //    return Accepted(product);
+        //}
+    }
+}
