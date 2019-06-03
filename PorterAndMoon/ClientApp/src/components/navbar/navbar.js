54604e62f7489915e1d0952623017a819edb4709
RichFisher <rich.fishernc@gmail.com>
2019-06-03 18:25:28 -0500

    other stuff

---
+++ PorterAndMoon/ClientApp/src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.scss';
+
+class navbar extends React.Component {
+  render() {
+    return(
+      <div className='navbar'>
+        
+      </div>
+    );
+  }
+}
+
+export default navbar;
