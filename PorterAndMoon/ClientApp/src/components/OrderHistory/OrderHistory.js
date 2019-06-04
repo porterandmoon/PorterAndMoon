bbc094b75c4ad69a7e6f8b1c0b8c3304a5e3412c
WaysideTester <dylanmurray211@outlook.com>
2019-06-03 21:18:31 -0500

    wip

--- PorterAndMoon/ClientApp/src/components/OrderHistory/OrderHistory.js
+++ PorterAndMoon/ClientApp/src/components/OrderHistory/OrderHistory.js
@@ -4,7 +4,7 @@ import './OrderHistory.scss';
 class OrderHistory extends React.Component {
   render() {
     return(
-      <div>
+      <div className="order-container">
         hello
       </div>
     );
