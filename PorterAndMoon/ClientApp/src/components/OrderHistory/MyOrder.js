27b73ee4266a7f06067defc2d2e9969cddc5995a
WaysideTester <dylanmurray211@outlook.com>
2019-06-04 20:48:31 -0500

    populating info on cards

---
+++ PorterAndMoon/ClientApp/src/components/OrderHistory/MyOrder.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class MyOrder extends React.Component {
+
+  render() {
+
+    return (
+      <div className="card">
+        <div className="card-body">
+          <p className="card-text">Date: {this.props.order.creationDate}</p>
+          <p className="card-text">Refunded {this.props.order.isRefunded}</p>
+          <p className="card-text">Itemization w/ quantity and product</p>
+          <p className="card-text">Payment info redacted</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MyOrder;
--- PorterAndMoon/ClientApp/src/components/OrderHistory/OrderHistory.js
+++ PorterAndMoon/ClientApp/src/components/OrderHistory/OrderHistory.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import OrderHistoryList from '../../data/PortAndMoonFactory/OrderHistory';
+import MyOrder from './MyOrder';
 import './OrderHistory.scss';
 
 class OrderHistory extends React.Component {
   state = {
-    orders: undefined
+    orders: []
   }
 
   componentDidMount() {
@@ -17,10 +18,20 @@ class OrderHistory extends React.Component {
       .catch(err => console.error(err));
   }
 
+  
   render() {
+
+    const listOrders = () => {
+      const orders = this.state.orders;
+      if(orders.length > 0) {
+        return orders.map(order => <MyOrder order={order} key={order.id}/>);
+      }
+    }
+    
     return(
       <div className="order-container">
         hello
+        { listOrders() }
       </div>
     );
   }
