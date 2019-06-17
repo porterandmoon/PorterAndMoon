dd7ae3a6a1fc27463cd5b14dde9c8e8b96589271
unknown <rys.mary@gmail.com>
2019-06-17 18:48:25 -0500

    rich fixed it

--- PorterAndMoon/ClientApp/src/components/SearchBar/searchDetail.js
+++ PorterAndMoon/ClientApp/src/components/SearchBar/searchDetail.js
@@ -1,29 +1,29 @@
 import React from 'react';
 
 class SearchDetail extends React.Component {
-    render() {
+    renderSearch = () => {
         const { searchData } = this.props;
-
-        const renderSearch = () => {
-            if (searchData.username !== 'null') {
-                return <h3>{searchData.username}</h3>
-            } else if (searchData.title !== 'null') {
-                return <div>
-                    <h3>{searchData.title}</h3>
-                    <p>{searchData.description}</p>
-                    <p>{searchData.price}</p>
-                    <p>{searchData.quantitiy}</p>
-                    </div>
-            } else {
-                return ''
-            };   
-        };
+        console.log(searchData);
+        if (searchData.username !== null) {
+            return <h3>{searchData.username}</h3>
+        } else if (searchData.title !== null ) {
+            return <div>
+                <h3>{searchData.title}</h3>
+                <p>{searchData.description}</p>
+                <p>{searchData.price}</p>
+                <p>{searchData.quantitiy}</p>
+                </div>
+        } else {
+            return ''
+        };   
+    };
+    render() {
 
         return (
             <div className="searchDetail">
                 <div className="container">
                     <div className="col">
-                        {renderSearch()}
+                        {this.renderSearch()}
                     </div>
                 </div>
             </div>
