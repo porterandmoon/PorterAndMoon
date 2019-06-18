6828eea6f5326ccaa04facddba6debf901971a73
WaysideTester <dylanmurray211@outlook.com>
2019-06-17 19:34:24 -0500

    wip

--- PorterAndMoon/ClientApp/src/components/Cart/ItemInCart.js
+++ PorterAndMoon/ClientApp/src/components/Cart/ItemInCart.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  Toast,
+  ToastBody,
+  ToastHeader,
+} from 'reactstrap';
 
 class ItemInCart extends React.Component {
   render() {
@@ -15,20 +20,23 @@ class ItemInCart extends React.Component {
       type,
     } = this.props.cartItem
     return(
-      <div>
-        <p>
-          <h3>arrival</h3><h2>{arrival}</h2>
-          <h3>departure</h3><h2>{departure}</h2>
-          <h3>description</h3><h2>{description} </h2>
+      <Toast>
+        <ToastHeader>
+          <span>departure</span><h2>{departure}</h2>
           <h3>destination</h3><h2>{destination} </h2>
+        </ToastHeader>
+        <ToastBody>
+          <h3>departure</h3><h2>{departure}</h2>
+          <h3>arrival</h3><h2>{arrival}</h2>
+          <h3>title</h3><h2>{title} </h2>
+          <h3>description</h3><h2>{description} </h2>
+          <h3>origin</h3><h2>{origin}</h2>
           <h3>isAvailable</h3><h2>{isAvailable}</h2>
-          <h3>origin</h3> <h2>{origin}</h2>
           <h3>quantity</h3><h2>{quantity} </h2>
           <h3>remainingQty</h3><h2>{remainingQty} </h2>
-          <h3>title</h3><h2>{title} </h2>
           <h3>type</h3><h2>{type}</h2>
-        </p>
-      </div>
+        </ToastBody>
+      </Toast>
     );
   }
 }
