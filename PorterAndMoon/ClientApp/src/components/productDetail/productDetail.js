5123c548d651e5895618153fe7e9ee98424d70b2
unknown <rys.mary@gmail.com>
2019-06-24 21:04:16 -0500

    updated search page and styled products page

--- PorterAndMoon/ClientApp/src/components/productDetail/productDetail.js
+++ PorterAndMoon/ClientApp/src/components/productDetail/productDetail.js
@@ -5,16 +5,15 @@ class ProductDetail extends React.Component {
 
     buildProduct = () => {
         const product = this.props;
-        console.log(product);
         if (product !== null) {
         return <div className="productDetails">
             <div className="container">
               <div className="col">
-                <h1>Flight Details</h1>
-                <h3>{product.product.title}</h3>
+                <h1>Details for Flight {product.product.title}</h1>
                 <p>{product.product.description}</p>
-                <p>${product.product.price}</p>
-                <p>Qty: {product.product.remainingQty}</p>
+                <p>Departing from {product.product.origin} on {product.product.departure} and Arriving at {product.product.destination} {product.product.arrival}</p>
+                <p>Ticket Price ${product.product.price}</p>
+                <p><i className="fas fa-ticket-alt"></i>  Qty Available: {product.product.remainingQty}</p>
                 </div>
             </div>
             </div>
