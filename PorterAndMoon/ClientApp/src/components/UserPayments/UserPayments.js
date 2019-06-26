615cc102d1cc2214823122aeed6b0f56a41f01c9
unknown <rys.mary@gmail.com>
2019-06-25 21:21:43 -0500

    wrap up

--- PorterAndMoon/ClientApp/src/components/UserPayments/UserPayments.js
+++ PorterAndMoon/ClientApp/src/components/UserPayments/UserPayments.js
@@ -7,33 +7,49 @@ class UserPayments extends React.Component {
     render(){
 
         return(<div>
-                <h1>UserPayments</h1>
-                    <Form>
-                    <FormGroup>
-                    <Label>Name</Label>
-                    <Input type="text" id="cardName" placeholder="Name on card" />
-                    </FormGroup>
-                    <FormGroup>
-                    <Label>Card Number</Label>
-                    <Input type="text"id="cardName" placeholder="#" />
-                    </FormGroup>
-                    <FormGroup>
-                    <Label for="exampleSelect">Select Payment Type</Label>
-                    <Input type="select" name="select" id="exampleSelect">
-                        <option>Visa</option>
-                        <option>MasterCard</option>
-                        <option>American Express</option>
-                        <option>Paypal</option>
-                    </Input>
-                    </FormGroup>  
-                    <FormGroup check>
-                    <Label check>
-                        <Input type="checkbox" />
-                        Make my preferred payment type
-                    </Label>
-                    </FormGroup>
-                    <Button>Submit</Button>
-                </Form>
+                <div className="container">
+                    <div className="row">
+                        <div className="userCards">
+                            <h3>Your Saved Payment Methods</h3>
+                        </div>
+                        <div className="cardForm">
+                            <Form>
+                            <FormGroup>
+                            <Label>Name</Label>
+                            <Input type="text" id="cardName" placeholder="Name on card" />
+                            </FormGroup>
+                            <FormGroup>
+                            <Label>Card Number</Label>
+                            <Input type="text"id="cardName" placeholder="#" />
+                            </FormGroup>
+                            <FormGroup>
+                            <Label>Security Number</Label>
+                            <Input type="text"id="securityNumber" placeholder="#" />
+                            </FormGroup>
+                            <FormGroup>
+                            <Label>Month/Year Expiration</Label>
+                            <Input type="month"id="cardDate" placeholder="" />
+                            </FormGroup>
+                            <FormGroup>
+                            <Label for="exampleSelect">Select Payment Type</Label>
+                            <Input type="select" name="select" id="exampleSelect">
+                                <option>Visa</option>
+                                <option>MasterCard</option>
+                                <option>American Express</option>
+                                <option>Paypal</option>
+                            </Input>
+                            </FormGroup>  
+                            <FormGroup check>
+                            <Label check>
+                                <Input type="checkbox" />
+                                Make my preferred payment type
+                            </Label>
+                            </FormGroup>
+                            <Button>Submit</Button>
+                        </Form>
+                        </div>
+                    </div>
+                </div>
         </div>);
     }
 }
